fa9db96476fba53b8b21b3f2fded219cdb0924e6
Rodrigo Rivera <rod45ri@gmail.com>
2026-02-07 01:10:07 -0600

    Agregando BD

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import os
 app = Flask(__name__)
 
 # 🔧 CONFIGURA TUS DATOS DE CONEXIÓN AQUÍ
-import os
 
-app.config['MYSQL_HOST'] = os.environ.get('metro.proxy.rlwy.net')
-app.config['MYSQL_PORT'] = int(os.environ.get('3815'))
-app.config['MYSQL_USER'] = os.environ.get('root')
-app.config['MYSQL_PASSWORD'] = os.environ.get('OWEyYfNTAQQeVBwzJorCncfzHzuEZPEt')
-app.config['MYSQL_DB'] = os.environ.get('railway')
 
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 mysql = MySQL(app)
 
 @app.route('/')
